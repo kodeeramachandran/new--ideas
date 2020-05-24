08d83151a2977975bc6156f9d98d07ab76b378a6
kodeeramachandran <koteeswaran.ramachandran@sixt.com>
2020-05-24 11:16:35 +0200

    rtc

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -20,27 +20,27 @@ export default {
     getQString(url='', keyToReturn=''){
         url = url ? url : location.href;
         let queryStrings = decodeURIComponent(url).split('#', 2)[0].split('?', 2)[1];
-        
+
         if(queryStrings){
             let splittedQStrings = queryStrings.split('&');
-            
+
             if(splittedQStrings.length){
                 let queryStringObj = {};
-                
+
                 splittedQStrings.forEach(function(keyValuePair){
                     let keyValue = keyValuePair.split('=', 2);
-                    
+
                     if(keyValue.length){
                         queryStringObj[keyValue[0]] = keyValue[1];
                     }
-                });            
-                
+                });
+
                 return keyToReturn ? (queryStringObj[keyToReturn] ? queryStringObj[keyToReturn] : null) : queryStringObj;
             }
-            
+
             return null;
         }
-        
+
         return null;
     },
 
@@ -53,7 +53,7 @@ export default {
     getUserFullMedia(){
         if(this.userMediaAvailable()){
             return navigator.mediaDevices.getUserMedia({
-                video: true, 
+                video: true,
                 audio: {
                     echoCancellation: true,
                     noiseSuppression: true
@@ -111,22 +111,20 @@ export default {
         // "turn:eu-turn4.xirsys.com:3478?transport=udp",
         return {
             iceServers: [
-                {
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                }, 
-                {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl", 
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
-                    urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",                         
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
-                    ]
-                }
-            ]
+                {urls: ["turn:173.194.72.127:19305?transport=udp",
+                   "turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
+                   "turn:173.194.72.127:443?transport=tcp",
+                   "turn:[2404:6800:4008:C01::7F]:443?transport=tcp"
+                   ],
+                 username:"CKjCuLwFEgahxNRjuTAYzc/s6OMT",
+                 credential:"u1SQDR/SQsPQIxXNWQT7czc/G4c="
+                },
+                {urls:["stun:stun.l.google.com:19302"]}
+              ]
         };
     },
-    
-    
+
+
     addChat(data, senderType){
         let chatMsgDiv = document.querySelector('#chat-messages');
         let contentAlign = 'justify-content-end';
@@ -183,7 +181,7 @@ export default {
 
     replaceTrack(stream, recipientPeer){
         let sender = recipientPeer.getSenders ? recipientPeer.getSenders().find(s => s.track && s.track.kind === stream.kind) : false;
-        
+
         sender ? sender.replaceTrack(stream) : '';
     },
 
@@ -191,7 +189,7 @@ export default {
 
     toggleShareIcons(share){
         let shareIconElem = document.querySelector('#share-screen');
-        
+
         if(share){
             shareIconElem.setAttribute('title', 'Stop sharing screen');
             shareIconElem.children[0].classList.add('text-primary');
@@ -257,8 +255,8 @@ export default {
 
     toggleModal(id, show){
         let el = document.getElementById(id);
-        
-        if(show){             
+
+        if(show){
             el.style.display = 'block';
             el.removeAttribute('aria-hidden');
         }
@@ -273,7 +271,7 @@ export default {
 
     setLocalStream(stream, mirrorMode=true){
         const localVidElem = document.getElementById('local');
-        
+
         localVidElem.srcObject = stream;
         mirrorMode ? localVidElem.classList.add('mirror-mode') : localVidElem.classList.remove('mirror-mode');
     },
@@ -308,7 +306,7 @@ export default {
 
         let testInterval = setInterval(()=>{
             let newVid = document.createElement('video');
-            newVid.id = `demo-${i}-video`;            
+            newVid.id = `demo-${i}-video`;
             newVid.srcObject = str;
             newVid.autoplay = true;
             newVid.className = 'remote-video';
@@ -318,14 +316,14 @@ export default {
             controlDiv.className = 'remote-video-controls';
             controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
                 <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
-            
+
             //create a new div for card
             let cardDiv = document.createElement('div');
             cardDiv.className = 'card card-sm';
             cardDiv.id = `demo-${i}`;
             cardDiv.appendChild(newVid);
             cardDiv.appendChild(controlDiv);
-            
+
             //put div in main-section elem
             document.getElementById('videos').appendChild(cardDiv);
 
